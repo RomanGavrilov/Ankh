82ecb2af11d4ca7e280efd6ad145da59436e7e68
Roman Gavrilov <gavr.rg@gmail.com>
2025-11-23 21:06:54 +0100

    command pool non-copyable

--- src/commands/command-pool.hpp
+++ src/commands/command-pool.hpp
@@ -8,6 +8,8 @@ namespace ankh
     {
     public:
         CommandPool(VkDevice device, uint32_t queueFamilyIndex);
+        CommandPool(const CommandPool&) = delete;
+        CommandPool& operator=(const CommandPool&) = delete;
         ~CommandPool();
 
         VkCommandPool handle() const;
